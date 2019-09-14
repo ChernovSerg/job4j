31ccc37890c1bcf4003b4e4211cb0c3bddc845be
Sergey Chernov <chernov_serg@mail.ru>
2019-09-14 09:09:49 +0300

    169973 Fix SqMax and tests for it.

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,9 +3,9 @@ package ru.job4j.condition;
 public class MultiMax {
     public int max(int first, int second, int third) {
         int max = first;
-        if (second > max) {
+        if (second > max && second > third) {
             max = second;
-        } else if (third > max) {
+        } else if (third > max && third > second) {
             max = third;
         }
         return max;
