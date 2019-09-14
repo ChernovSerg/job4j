d51da8c2fa70ca196fdbcfefefc1746062d798a0
Sergey Chernov <chernov_serg@mail.ru>
2019-09-14 23:08:29 +0300

    169983 Class PrimeNumber and tests for it.

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        if (finish > 1) {
+            for (int i = 2; i <= finish; i++) {
+                boolean remainder = true;
+                int j;
+                for (j = 2; j <= i; j++) {
+                    if (i % j == 0) {
+                        remainder = false;
+                        break;
+                    }
+                }
+                if (remainder == false && j == i) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
