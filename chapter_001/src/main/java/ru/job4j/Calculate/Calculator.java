5940f5112826397c32741762d1bcb4ac90253378
Sergey Chernov <chernov_serg@mail.ru>
2019-09-15 07:43:45 +0300

    169980 Javadocs for package and class Calculator.

--- chapter_001/src/main/java/ru/job4j/Calculate/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/Calculate/Calculator.java
@@ -1,21 +1,50 @@
-package ru.job4j.part_01;
+package ru.job4j.Calculate;
 
+/**
+ * It is simple calculator.
+ * @author Сергей Ч
+ *
+ */
 public class Calculator {
+    /**
+     * Method adds two arguments and prints them.
+     * @param first
+     * @param second
+     * @return nothing
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
 
+    /**
+     * Method divides two arguments and prints them.
+     * @param first
+     * @param second
+     * @return nothing
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(result);
     }
 
+    /**
+     * Method multiplies two arguments and prints them.
+     * @param first
+     * @param second
+     * @return nothing
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(result);
     }
 
+    /**
+     * Method subtracts two arguments and prints them.
+     * @param first
+     * @param second
+     * @return nothing
+     */
     public static void subtrack(double first, double second) {
         double result = first - second;
         System.out.println(result);
---
+++ chapter_001/src/main/java/ru/job4j/Calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Description for the package ru.job4j.Calculate
+ * @author Сергей Ч
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.Calculate;
