c95acd0fba36e873d55ebcd03ecaa255fd57170a
Sergey Chernov <chernov_serg@mail.ru>
2019-09-16 20:43:59 +0300

    169998 Add overload method "indexOf" and tests for it and javadoc comments to the FindLoop class.

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,12 +1,41 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    /**
+     * Method search element position from array.
+     *
+     * @param data array of integer
+     * @param el   required element
+     * @return the position of the element found or -1 if the element is not found
+     */
     public int indexOf(int[] data, int el) {
-        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * Method search element position from array from start position to end position.
+     *
+     * @param data   origin array
+     * @param el     required element
+     * @param start  start position for search, starts at 0
+     * @param finish end position for search, ends with a value equal to the size-1 of the origin array
+     * @return the position of the element found or -1 if the element is not found
+     */
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        if (data.length > start && data.length > finish) {
+            for (int index = start; index <= finish; index++) {
+                if (data[index] == el) {
+                    rst = index;
+                    break;
+                }
             }
         }
         return rst;
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -12,7 +12,7 @@ public class FindLoopTest {
         int[] array = {2, -3, 5, 8, -2};
         FindLoop find = new FindLoop();
         int pos = find.indexOf(array, 8);
-        assertThat(pos,is(3));
+        assertThat(pos, is(3));
     }
 
     @Test
@@ -20,6 +20,73 @@ public class FindLoopTest {
         int[] array = {2, -3, 5, 8, -2};
         FindLoop find = new FindLoop();
         int pos = find.indexOf(array, 6);
-        assertThat(pos,is(-1));
+        assertThat(pos, is(-1));
     }
+
+    @Test
+    public void whenGoodFindAtFirstArray() {
+        int[] input = new int[]{5, 10, 3};
+        int value = 5;
+        int result = FindLoop.indexOf(input, value, 0, 2);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenGoodFindInsideArray() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+
+    @Test
+    public void whenGoodFindInEndArray() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 4;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenElementAbsentInArray() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 14;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenBadStartPosition() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 6;
+        int finish = 3;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenBadFinishPosition() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 0;
+        int finish = 8;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+
 }
