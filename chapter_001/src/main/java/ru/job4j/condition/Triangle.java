8d18ac49bff8c7a875ef3539aad7af1ef88254f9
Sergey Chernov <chernov_serg@mail.ru>
2019-10-06 09:57:07 +0300

    170099 refactored class Triangle

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,21 +1,67 @@
 package ru.job4j.condition;
 
+import ru.job4j.part01.Point;
+
 public class Triangle {
+    Point p1;
+    Point p2;
+    Point p3;
+
+    public Triangle(Point p1, Point p2, Point p3) {
+        if (this.exist(p1, p2, p3)) {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+        }
+    }
+
     /**
      * Determines if a triangle exists with such sides
-     * @param ab length of the first side of the triangle
-     * @param bc length of the second side of the triangle
-     * @param ac length of the third side of the triangle
-     * @return
+     *
+     * @return true or false
      */
-    public static boolean exist(double ab, double ac, double bc) {
+    private boolean exist(Point p1, Point p2, Point p3) {
+        double ab = p1.distance(p2);
+        double bc = p2.distance(p3);
+        double ac = p1.distance(p3);
         boolean result = false;
-        if ((ab + ac) > bc
-                && (ab + bc) > ac
-                && (bc + ac) > ab
-        ) {
+        if ((ab + ac) > bc && (ab + bc) > ac && (bc + ac) > ab) {
             result = true;
         }
         return result;
+    }
+
+    public boolean exist() {
+        return this.p1 != null;
+    }
+
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     *
+     * @return Периметр.
+     */
+    public double perimeter() {
+        return this.p1.distance(this.p2) + this.p2.distance(this.p3) + this.p1.distance(this.p3);
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     * <p>
+     * Формула:
+     * √ p *(p - ab) * (p - ac) * (p - bc)
+     * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
+     *
+     * @return Вернуть площадь, если треугольник существует или -1.
+     */
+    public double area() {
+        double rsl = -1;
+        if (this.exist()) {
+            double p = this.perimeter() / 2;
+            double ab = this.p1.distance(this.p2);
+            double bc = this.p2.distance(this.p3);
+            double ac = this.p1.distance(this.p3);
+            rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+        return rsl;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,22 +1,42 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+import ru.job4j.part01.Point;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 
 public class TriangleTest {
 
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(0, 2), new Point(2, 0));
+        boolean result = triangle.exist();
         assertThat(result, is(true));
     }
 
     @Test
     public void whenNotExist() {
-        boolean result = Triangle.exist(2.0, 4.1, 2.0);
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(0, 2), new Point(0, -1));
+        boolean result = triangle.exist();
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void calcPerimeter() {
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(0, 1), new Point(1, 0));
+        int actual = (int) (triangle.perimeter() * 10000);
+        assertThat(actual, is(34142));
+    }
+
+    @Test
+    public void calcArea() {
+        Triangle triangle = new Triangle(new Point(0, 0), new Point(0, 1), new Point(1, 0));
+        double expected_area = 0.5;
+        double actual_area = triangle.area();
+        boolean expected = Math.abs(expected_area - actual_area) < 0.1e6;
+        assertTrue(expected);
     }
 }
