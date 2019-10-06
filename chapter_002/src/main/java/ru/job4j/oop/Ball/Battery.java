a7df15bb19d3ee40eccc8f1567d03c4341bc7026
Sergey Chernov <chernov_serg@mail.ru>
2019-10-06 07:51:30 +0300

    170109 class Battery

---
+++ chapter_002/src/main/java/ru/job4j/oop/Ball/Battery.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop.Ball;
+
+public class Battery {
+    private int load;
+    Battery(int volt) {
+        this.load = volt;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load - 1;
+        another.load += 1;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(12);
+        Battery second = new Battery(10);
+        System.out.println("first:" + first.load + ". second: " + second.load);
+        first.exchange(second);
+        System.out.println("first:" + first.load + ". second: " + second.load);
+    }
+}
