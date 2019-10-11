5eb1f5ce8d4580d45af596bbd3fa5be8352ac099
Sergey Chernov <chernov_serg@mail.ru>
2019-10-11 08:06:01 +0300

    170114 Tracker and Item

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,93 @@
 package ru.job4j.tracker;
 
+import java.util.Random;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int position = 0;
+
+    private String generateId() {
+        Random rnd = new Random();
+        return String.valueOf(rnd.nextLong() + System.currentTimeMillis());
+    }
+
+    private int getIndexById(String id) {
+        int result = -1;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && items[i].getId().equals(id)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private int getIndexByName(String id) {
+        int result = -1;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && items[i].getName().equals(id)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    public Item findById(String id) {
+        Item result = null;
+        int position = getIndexById(id);
+        if (position >= 0) {
+            result = items[position];
+        }
+        return result;
+    }
+
+    public boolean replace(String id, Item item) {
+        boolean result = false;
+        int position = getIndexById(id);
+        if (position >= 0) {
+            items[position] = item;
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean delete(String id) {
+        boolean result = false;
+        int position = getIndexById(id);
+        if (position >= 0) {
+            System.arraycopy(items, position + 1, items, position, items.length - position - 1);
+            result = true;
+        }
+        return result;
+    }
+
+    public Item findByName(String name) {
+        Item result = null;
+        int position = getIndexByName(name);
+        if (position >= 0) {
+            result = items[position];
+        }
+        return result;
+    }
+
+    public Item[] findAll() {
+        Item[] preResult = new Item[this.items.length];
+        int count = 0;
+        for(int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null) {
+                preResult[count++] = this.items[i];
+            }
+        }
+        Item[] result = new Item[count];
+        System.arraycopy(preResult, 0, result, 0, count);
+        return result;
+    }
+
 }
