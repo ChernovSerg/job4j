530b7057e34c646c87e806985703402216044a92
Sergey Chernov <chernov_serg@mail.ru>
2019-11-08 20:58:19 +0300

    170056 ConvertMatrix2List. Используем форич

--- chapter_003/src/main/java/ru/job4j/search/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/job4j/search/ConvertMatrix2List.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class ConvertMatrix2List {
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                list.add(Integer.valueOf(array[i][j]));
+        for (int[] row : array) {
+            for (int e : row) {
+                list.add(Integer.valueOf(e));
             }
         }
         return list;
