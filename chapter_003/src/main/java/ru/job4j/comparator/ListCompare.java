468a786af49c89543db0e014fa575a3d9e9255ff
Sergey Chernov <chernov_serg@mail.ru>
2019-11-13 08:17:17 +0300

    170045 Comparator: only start. To be continued.

---
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,11 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+public class ListCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return 0;
+    }
+}
