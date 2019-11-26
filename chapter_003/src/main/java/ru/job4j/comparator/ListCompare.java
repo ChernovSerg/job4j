81dbd9d411a6f77fda1171e10268aa70e9889cdb
Sergey Chernov <chernov_serg@mail.ru>
2019-11-27 00:35:21 +0300

    170045 Comparator for string.

--- chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -6,6 +6,22 @@ public class ListCompare implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        return 0;
+        int result = 0;
+        if (o1.length() > 0 && o2.length() > 0) {
+            int i = 0;
+            while (i < o1.length() && i < o2.length()) {
+                if (o1.charAt(i) != o2.charAt(i)) {
+                    result = o1.charAt(i) > o2.charAt(i) ? 1 : -1;
+                    break;
+                }
+                i++;
+            }
+            if (result == 0 && (o1.length() != o2.length())) {
+                result = o1.length() > i ? 1 : -1;
+            }
+        } else if (o1.length() > 0 || o2.length() > 0) {
+            result = o1.length() > o2.length() ? 1 : -1;
+        }
+        return result;
     }
 }
