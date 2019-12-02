a348ec195e47489b970513ba8c1c6ce24387fc97
Sergey Chernov <chernov_serg@mail.ru>
2019-12-02 08:11:50 +0300

    gitignore update

--- chapter_003/src/main/java/ru/job4j/banktransfers/Account.java
+++ chapter_003/src/main/java/ru/job4j/banktransfers/Account.java
@@ -1,4 +1,54 @@
 package ru.job4j.banktransfers;
 
+import java.util.Objects;
+
 public class Account {
+    private Double value;
+    private String requisites;
+
+    public Account(String requisites) {
+        this.requisites = requisites;
+        this.value = 0.0;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getRequisites() {
+        return requisites;
+    }
+
+    public void setRequisites(String requisites) {
+        this.requisites = requisites;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Account account = (Account) o;
+        return requisites.equals(account.requisites);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requisites);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{"
+                + " value=" + value
+                + ", requisites='" + requisites + '\''
+                + '}';
+    }
 }
--- chapter_003/src/main/java/ru/job4j/banktransfers/Bank.java
+++ chapter_003/src/main/java/ru/job4j/banktransfers/Bank.java
@@ -1,4 +1,8 @@
 package ru.job4j.banktransfers;
 
+import java.util.List;
+import java.util.Map;
+
 public class Bank {
+    Map<User, List<Account>> usersAccounts;
 }
